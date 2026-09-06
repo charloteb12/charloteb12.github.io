8114ee65527dc92109ad193c225efbaaf2cbb0e0
Charlote Anog <80906647+charloteb12@users.noreply.github.com>
2026-09-07 01:49:11 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("a[href='#']").forEach(link => {
+    link.addEventListener("click", e => e.preventDefault());
+  });
+});
